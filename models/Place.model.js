c56569f4826c0c7a0e9f0db4d21bd8842dcd1be1
lilyVolo <lvolotchko@gmail.com>
2023-08-26 19:01:32 +0200

    +

---
+++ models/Place.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const placesSchema = new Schema(
+  {
+  
+formatted_address: String,
+geometry: Object,
+name: String,
+photos: String,
+rating: Number,
+types: Array,
+user_ratings_total: Number,
+services: Array,
+budget: String,
+city: { type: mongoose.Schema.Types.ObjectId,
+    ref: "City" }
+  },
+
+);
+
+const Place = model("Place", placesSchema);
+
+module.exports = Place;
--- routes/cities.routes.js
+++ routes/cities.routes.js
@@ -1,6 +1,12 @@
-const City = require("../models/City.model")
+const City = require("../models/City.model");
+const Place = require("../models/Place.model");
+const mangoose = require('mongoose');
+const ObjectId = mangoose.Types.ObjectId;
 
 const router = require("express").Router()
+
+//router.use("/:cityId/places", require("./places.routes"))
+
 
 router.get("/", async (req, res, next) => {
     try {
@@ -11,7 +17,7 @@ router.get("/", async (req, res, next) => {
 	}
   });
 
-  router.get("/:cityId", (req, res, next) => {
+  router.get("/:cityId", async (req, res, next) => {
     const cityId = req.params.cityId;
   City.findById(cityId)
     .then((targetCity) => {
@@ -22,5 +28,32 @@ router.get("/", async (req, res, next) => {
       next(error)
     });
 });
+
+router.get("/:cityId/places", async (req, res, next) => {
+  const cityId = req.params.cityId;
+	const option1 = req.query.option1;
+  const option2 = req.query.option2;
+  
+  console.log(req.query)
+  const cityIndf = new ObjectId(cityId)
+  try {
+    const place1 = await Place.find({ //findOne
+      services: option1, 
+      city: cityIndf
+    })
+    const place2 = await Place.find({ //findOne
+      services: option2, 
+      city: cityIndf
+    })  
+    console.log(option2)
+    return res.status(200).json([place1, place2]);
+  }
+  catch(error) {
+    next(error)
+  }
+    
+})
+
+
 
   module.exports = router;
