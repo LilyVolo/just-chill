ce488a5c72c73801a20e31bdba3870d5fc084220
lilyVolo <lvolotchko@gmail.com>
2023-12-16 13:39:22 +0100

    Revert "+"
    This reverts commit bddce87b6e4bc65aaaf17e251a644b2b9861a800.

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.use(cors());
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-//  Start handling routes here
+// 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 
 
-// To handle errors. Routes that don't exist or errors that you handle in specific routes
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
